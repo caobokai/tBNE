fcbf3ce14068fd07d071e065c1ab1c2bc36236eb
Bokai Cao <caobokai@qq.com>
2018-02-25 00:52:41 -0600

    Add files via upload

---
+++ bne_demo.m
@@ -0,0 +1,17 @@
+clear all
+clc
+
+addpath(genpath('./tensor_toolbox'));
+addpath(genpath('./FOptM'));
+rng(5489, 'twister');
+
+m = 10;
+n = 10;
+k = 10; % rank for tensor
+[X, Z, Y] = bne_data(m, n, k); % generate the tensor, guidance and label
+
+[T, W] = bne_fun(X, Z, Y, k);
+
+[~, y1] = max(Y, [], 2);
+[~, y2] = max(T{3} * W, [], 2);
+fprintf('accuracy %3.2e\n', sum(y1==y2)/n);
